ad25c3b7e53acba3b586d8d2342b56deaecbaae9
Kohuke123 <sandra1pllu@gmail.com>
2025-04-15 22:10:34 +0300

    adding titles

--- main.py
+++ main.py
@@ -14,16 +14,37 @@ def write_data(content):
         file.write(json.dumps(content)) #Saves and turns python data into a json string
     return content
 
-# @eel.expose
-# Function to create a new note
-# def create_note(title):
-#     global notes_count
+@eel.expose
+#Function to create a new note
+def create_note(title):
+    global notes_count
 
-#     new_note = { # This dictionary assings a new id to every note
-#         "id": notes_count + 1
-#         "title": title
-#     }
+    new_note = { # This dictionary assings a new id to every note
+        "id": notes_count + 1,
+        "title": title
+    }
 
-#     content = read_data()
+    content = read_data()
+    content['notes'].append(new_note) # Adds the new note to the list
 
+    write_data(content) # Saving the updated list
+    notes_count += 1 # Next note has a new id
+
+    return new_note
+
+@eel.expose
+def list_note(): # Lists all the notes in the json file
+    return read_data()
+
+# Creating a new json file when first starting the app
+import os
+if not os.path.exists('data.json'):
+    file = open("data.json", "w")
+    file.write(json.dumps({"notes":[]})) # Empy list of notes
+    file.close()
+
+else:
+    content = read_data()
+    note_count = len(content['notes']) # Counts the existing notes
+    
 eel.start('index.html', size=(300, 400))
--- web/script.js
+++ web/script.js
@@ -1,1 +1,34 @@
-const note-title-input = document.getElementById("note-title-input") 
+const note_title_input = document.getElementById("note-title-input")
+const note_add_button = document.getElementById("note-add-button")
+const notes_table_body = document.getElementById("notes-table-body")
+
+eel.expose
+function displayNote(note) {
+    let tr = document.createElement("tr") // Tablerow
+    let td = document.createElement("td") // Title cell
+    td.innerText = note['title'];
+
+    tr.appendChild(td);
+    notes_table_body.appendChild(tr);
+
+    note_title_input.value = "";
+}
+
+// Function to display all the notes
+eel.expose
+function displayAllNotes(notes) {
+    for (let note of notes["notes"]) {
+        displayNote(note);
+    }
+}
+
+// Clicking the Add button
+note_add_button.addEventListener("click", (event) => { // When the user clicks on the add button
+    let content = note_title_input.value; // Gets the title and calls py fun to store note to json file
+    if (content != "") { // Checks if the input is empty or not, then saves it and displays notes
+        eel.create_note(content)(displayNote)
+    }
+
+})
+
+eel.list_note()(displayAllNotes)
