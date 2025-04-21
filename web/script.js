1c657c7bb322c1e40abcdad24bb9b76d8a68931d
Kohuke123 <sandra1pllu@gmail.com>
2025-04-21 11:11:44 +0300

    editor

--- web/script.js
+++ web/script.js
@@ -1,10 +1,10 @@
 const note_title_input = document.getElementById("note-title-input")
 const note_add_button = document.getElementById("note-add-button")
 const notes_table_body = document.getElementById("notes-table-body")
+const note_id_title = document.getElementById("note-id-title")
 
 eel.expose
 function displayNote(note) {
-  console.log("Displaying note:", note);
   let tr = document.createElement("tr") // Tablerow
   let td = document.createElement("td") // Title cell
   td.innerText = note['title'];
@@ -34,6 +34,10 @@ function openNoteWindow(noteId) {
     `editor.html?noteId=${noteId}`,
     `NoteWindow-${noteId}`,
     "width=300,height=400");
+
+  let h = getElementById("h3");
+  h.innerText = note['title'];
+  note_id_title.appendChild(h);
 }
 
 // Clicking the Add button
